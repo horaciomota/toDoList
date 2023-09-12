f09037e7119f18ee86b39b4d6a48bba2e9aab05c
Horacio Mota <horaciomota93@gmail.com>
2023-09-12 17:01:38 +0100

    App wrapped in a navigation view

--- toDoApp/toDoAppApp.swift
+++ toDoApp/toDoAppApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct toDoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                
+                ListView()
+                
+            }
         }
     }
 }
