c5d527cb3605cfd38c6483398d53fbabd372cdac
Horacio Mota <horaciomota93@gmail.com>
2023-09-12 16:57:37 +0100

    Initial Commit

---
+++ toDoApp/toDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoAppApp.swift
+//  toDoApp
+//
+//  Created by Horacio Mota on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
